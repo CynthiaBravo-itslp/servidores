3f3eb32c1f2b8f8f8038419e1e0f27b994e74351
CynthiaBravo-itslp <bravocynthia8@gamil.com>
2020-04-30 21:05:43 -0500

    wordpress heroku

--- contenido.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "<h1> contenido de la materia servidores</h1> <br />";
-echo "<p> este es un ejercicio de la materia  de servidores que te mostrara  en heroku</p";
-    
-    
-    
- ?>
